c98c6ea6fcc94f4e8f1514dacc7a8951bf0debe5
Olivia-Melody <livmelyin@gmail.com>
2025-03-23 17:14:33 +0000

    Layout update2

--- MyWorld.java
+++ MyWorld.java
@@ -57,7 +57,7 @@ public class MyWorld extends World {
         }
         
         // 2nd left vertical wall
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             addObject(new wall(), 10, 160 + i * WALL_HEIGHT);
         }
         
@@ -71,21 +71,27 @@ public class MyWorld extends World {
             addObject(new wall(), 300, 170 + i * WALL_HEIGHT);
         }
         
+        // 5th vertical wall
+        for (int i = 0; i < 2; i++) {
+            addObject(new wall(), 80, 340 + i * WALL_HEIGHT);
+        }
+        
+        // 6th left vertical wall
+        for (int i = 0; i < 5; i++) {
+            addObject(new wall(), 10, 290 + i * WALL_HEIGHT);
+        }
+        
         // Right vertical wall
         for (int i = 0; i < 15; i++) {
             addObject(new wall(), 590, 30 + i * WALL_HEIGHT);
         }
 
         // Bottom horizontal wall
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < 22; i++) {
             addObject(new wall(), 10 + i * WALL_WIDTH, 390);
         }
         
-        // Bottom horizontal wall 2
-        for (int i = 0; i < 10; i++) {
-            addObject(new wall(), 400 + i * WALL_WIDTH, 390);
-        }
-
+        
         //small desk areas for office space
         addObject(new wall(), 178, 100);
         addObject(new wall(), 178, 126);
@@ -93,6 +99,8 @@ public class MyWorld extends World {
         addObject(new wall(), 207, 100);
         addObject(new wall(), 207, 126);
         addObject(new wall(), 207, 152);
+        addObject(new deskchair(), 178, 178);
+        addObject(new deskchair(), 207, 178);
         
         //small desk areas for office space
         addObject(new wall(), 178, 300);
@@ -112,8 +120,21 @@ public class MyWorld extends World {
         addObject(new wall(), 457, 152);
         addObject(new wall(), 428, 178);
         addObject(new wall(), 457, 178);
-        addObject(new wall(), 560, 200);
-        addObject(new wall(), 530, 200);
+        addObject(new wall(), 560, 170);
+        addObject(new wall(), 530, 170);
+        addObject(new deskchair(), 530, 200);
+        addObject(new deskchair(), 560, 200);
+        
+        //small desk areas for office space
+        addObject(new wall(), 500, 330);
+        addObject(new wall(), 470, 330);
+        addObject(new wall(), 440, 330);
+        addObject(new wall(), 410, 330);
+        addObject(new wall(), 380, 330);
+        addObject(new wall(), 350, 330);
+        addObject(new wall(), 320, 330);
+        addObject(new wall(), 290, 330);
+        
     }
 }
 
