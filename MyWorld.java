10db21df42b33a7253ad460533cc73284bd2f428
Olivia-Melody <livmelyin@gmail.com>
2025-03-23 14:32:44 +0000

    Maze 1 interface

--- MyWorld.java
+++ MyWorld.java
@@ -10,6 +10,8 @@ public class MyWorld extends World {
     
     private static final int WORLD_WIDTH = 600;
     private static final int WORLD_HEIGHT = 400;
+    private static final int WALL_WIDTH = 28;
+    private static final int WALL_HEIGHT = 26;
 
     public MyWorld() {
         // Initialize the world with a specific width and height
@@ -17,10 +19,50 @@ public class MyWorld extends World {
 
         TiredOfficeWorker worker = new TiredOfficeWorker();
         addObject(worker, 300, 200);
+        
+        layout();
 
     }
 
     public void removeCollectibleFromWorld(Collectible collectible) {
         removeObject(collectible);
     }
+    
+    private void layout() {
+        // Create office-like maze walls (desks, partitions, etc.)
+        
+        // Top horizontal wall
+        for (int i = 0; i < 25; i++) {
+            addObject(new wall(), 10 + i * WALL_WIDTH, 10);
+        }
+
+        // 1st left vertical wall
+        for (int i = 0; i < 3; i++) {
+            addObject(new wall(), 10, 30 + i * WALL_HEIGHT);
+        }
+        
+        // 2nd left vertical wall
+        for (int i = 0; i < 10; i++) {
+            addObject(new wall(), 10, 160 + i * WALL_HEIGHT);
+        }
+
+        // Right vertical wall
+        for (int i = 0; i < 15; i++) {
+            addObject(new wall(), 590, 30 + i * WALL_HEIGHT);
+        }
+
+        // Bottom horizontal wall
+        for (int i = 0; i < 25; i++) {
+            addObject(new wall(), 10 + i * WALL_WIDTH, 390);
+        }
+
+        // Create small desk areas to mimick office space
+        addObject(new wall(), 150, 100);
+        addObject(new wall(), 178, 100);
+        addObject(new wall(), 206, 100);
+        addObject(new wall(), 206, 126);
+        addObject(new wall(), 206, 152);
+    }
 }
+
+
