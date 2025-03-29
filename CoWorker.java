6931a9ecbd2aca186667a85137f45ee74a2247ed
alexelw <wattsalex2006@gmail.com>
2025-03-29 15:34:14 +0000

    co worker done

--- CoWorker.java
+++ CoWorker.java
@@ -12,6 +12,9 @@ public class CoWorker extends Worker {
     private final Point pointA;
     private final Point pointB; // Use shared chat interaction logic
 
+    private AnimationHandler animation;
+    private boolean chatting = false;
+
     public CoWorker(Point pointA, Point pointB) {
         super(new TwoPointMovement(pointA, pointB));
         ((TwoPointMovement) movement).setWorker(this);
@@ -19,24 +22,52 @@ public class CoWorker extends Worker {
         this.pointA = pointA;
         this.pointB = pointB;
         setSpeed(SPEED);
+        
+        animation = new AnimationHandler(this,
+            "coworker_idle.png",
+            new String[]{"coworker_walk1.png", "coworker_walk2.png"},
+            new String[]{"coworker_effect1.png", "coworker_effect2.png"},
+            new String[]{"coworker_talk1.png", "coworker_talk2.png"},
+            32, 32
+        );
+
 
     }
 
     @Override
     public void act() {
         move();
-
+        handleAnimation(); 
         TiredOfficeWorker worker = (TiredOfficeWorker) getOneIntersectingObject(TiredOfficeWorker.class);
         if (worker != null) {
             interactWith(worker);
         }
     }
 
+    
+        public void setChatting(boolean chatting) {
+        this.chatting = chatting;
+    }
+    
+    public void animateTalking() {
+        chatting = true;
+        animation.animateTalking();
+    }
+    
     @Override
     public void resumeMovement() {
-        setSpeed(SPEED);
+        chatting = false;
         ((TwoPointMovement) movement).resumeMoving();
     }
+    
+    private void handleAnimation() {
+        if (chatting) {
+            animation.animateTalking();
+        } else {
+            animation.animateWalking(); // Coworker is always moving
+        }
+    }
+
 
     @Override
     public void interactWith(Worker worker) {
@@ -52,6 +83,8 @@ public class CoWorker extends Worker {
     @Override
     public void stopMovement() {
         ((TwoPointMovement) movement).stopMoving();
+        chatting = true;
+        handleAnimation();
     }
 
     @Override
